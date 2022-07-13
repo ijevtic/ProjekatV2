2c8547c8e5e28d6f66007d9af66fe5a7ca1ee500
dev <ivanjevtic501@gmail.com>
2022-07-13 13:32:01 +0200

    deploy files

--- hardhat/contracts/MainContract.sol
+++ hardhat/contracts/MainContract.sol
@@ -4,11 +4,9 @@ pragma solidity ^0.8.9;
 // Import this file to use console.log
 import "hardhat/console.sol";
 
-
 error MainContract__StakerNotFound();
 
 contract MainContract {
-
     address private immutable i_owner;
 
     uint constant STAKE_TIME = 30 days;
@@ -30,53 +28,59 @@ contract MainContract {
         i_owner = msg.sender;
     }
 
-
     function stakeEther() public payable {
         User storage user = userStakeMapping[msg.sender];
         user.stakedEther += msg.value;
         totalSum += msg.value;
         user.startDate = block.timestamp;
-        (bool sent, bytes memory data) = payable(address(this)).call{value: msg.value}("");
+        (bool sent, bytes memory data) = payable(address(this)).call{
+            value: msg.value
+        }("");
 
-        if(!user.exists) {
+        if (!user.exists) {
             user.exists = true;
             users.push(msg.sender);
-            user.index = users.length-1;
+            user.index = users.length - 1;
         }
     }
-
 
     function extractEther() public {
         User storage user = userStakeMapping[msg.sender];
 
         uint index = user.index;
-        if(index >= users.length || !user.exists)
+        if (index >= users.length || !user.exists)
             revert MainContract__StakerNotFound();
-        
+
         user.exists = false;
-        users[index] = users[users.length-1];
+        users[index] = users[users.length - 1];
         userStakeMapping[users[index]].index = index;
         users.pop();
 
         totalSum -= user.stakedEther;
-        uint percentage = (block.timestamp - user.startDate) * MULTIPLY / STAKE_TIME / 2 + MULTIPLY / 2;
+        uint percentage = ((block.timestamp - user.startDate) * MULTIPLY) /
+            STAKE_TIME /
+            2 +
+            MULTIPLY /
+            2;
         console.log("procenat");
         console.log(percentage);
         uint withdrawAmount = user.stakedEther;
         console.log("staked ether");
         console.log(withdrawAmount);
-        if(percentage < MULTIPLY) {
+        if (percentage < MULTIPLY) {
             // uzmi procentualno
-            withdrawAmount = withdrawAmount * percentage / MULTIPLY;
+            withdrawAmount = (withdrawAmount * percentage) / MULTIPLY;
             uint remaining = user.stakedEther - withdrawAmount;
-            for(uint i = 0; i < users.length; i++) {
+            for (uint i = 0; i < users.length; i++) {
                 uint base = userStakeMapping[users[i]].stakedEther;
-                uint add = base * remaining / totalSum;
+                uint add = (base * remaining) / totalSum;
                 userStakeMapping[users[i]].stakedEther += add;
-            } 
+            }
         }
 
-        (bool sent, bytes memory data) = payable(msg.sender).call{value: withdrawAmount}("");
+        (bool sent, bytes memory data) = payable(msg.sender).call{
+            value: withdrawAmount
+        }("");
         console.log(withdrawAmount);
         // ethers = redeemAaveTokens(withdrawAmount);
         // transfer(msg.sender, ethers);
@@ -87,5 +91,7 @@ contract MainContract {
         require(i_owner == msg.sender);
     }
 
-
+    function test(uint number) public returns(uint) {
+        return number+1;
+    }
 }
---
+++ hardhat/scripts/sample-script.js
@@ -0,0 +1,13 @@
+
+// const hre = require("hardhat");
+
+
+// async function main() {
+//     const Ecommerce = await hre.ethers.getContractFactory("MainContract");
+//     const myEcommerce = await Ecommerce.deploy();
+//     await myEcommerce.deployed();
+//     console.log("MyFirstContract deployed to:", myEcommerce.address);
+
+// }
+
+// main()
